89e791372441c87355bc5beaffabbef1058cfe5a
Kadah-Ne <mgouviaux@gmail.com>
2024-08-16 10:58:37 +0200

    changements mineurs

--- API_COMMANDE/Commande/tests.py
+++ API_COMMANDE/Commande/tests.py
@@ -43,18 +43,3 @@ class CommandesTestCase(TestCase):
         self.assertEqual(Commandes.objects.all().count(), 2)
 
 
-# class ModelTests(TestCase):
-#     def testmanytomanyrelation(self):
-
-#         # Créez des instances de vos modèles
-#         produit1 = Produits.objects.create(id=1)
-#         produit2 = Produits.objects.create(id=2)
-#         commande = Commandes.objects.create(customerId=1)
-
-#         # Ajoutez les instances de Topping à la relation ManyToMany
-#         commande.products.add(produit1, produit2)
-
-#         # Vérifiez que la relation ManyToMany fonctionne correctement
-#         self.assertEqual(commande.products.count(), 2)
-#         self.assertTrue(produit1 in commande.products.all())
-#         self.assertTrue(produit2 in commande.products.all())
